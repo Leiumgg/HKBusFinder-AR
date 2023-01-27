84532b6b88f4ee4102127ab1ba180baf2faf1316
Leiumgg <chungkai615@gmail.com>
2023-01-28 04:37:55 +0800

    Initial Commit

---
+++ HKBusFinder-AR/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HKBusFinder-AR
+//
+//  Created by John Leung on 28/1/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HKBusFinder-AR/HKBusFinder_ARApp.swift
@@ -0,0 +1,17 @@
+//
+//  HKBusFinder_ARApp.swift
+//  HKBusFinder-AR
+//
+//  Created by John Leung on 28/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct HKBusFinder_ARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
